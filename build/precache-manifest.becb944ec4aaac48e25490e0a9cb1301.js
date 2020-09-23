0d2c517fa7ef5f043a5f1717e0f840a10d7b0f1e
RudeSoul <prabesh7@gmail.com>
2020-09-23 13:34:49 +0545

    added build files too

---
+++ build/precache-manifest.becb944ec4aaac48e25490e0a9cb1301.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69f9bac42f65a7e11d7b7253217c25c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fee0fd42b588778dde28",
+    "url": "/static/css/2.36a0b970.chunk.css"
+  },
+  {
+    "revision": "19ac6e4327bd507e4d7d",
+    "url": "/static/css/main.6ae7f92f.chunk.css"
+  },
+  {
+    "revision": "fee0fd42b588778dde28",
+    "url": "/static/js/2.98c2a6f6.chunk.js"
+  },
+  {
+    "revision": "3946f15296c2b303c2f2b22769e02415",
+    "url": "/static/js/2.98c2a6f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19ac6e4327bd507e4d7d",
+    "url": "/static/js/main.9a9942ef.chunk.js"
+  },
+  {
+    "revision": "d5bf5c702fd3977e87b3",
+    "url": "/static/js/runtime-main.f6fec446.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "8b50ef6f92f4fb5137031bda7cb0b659",
+    "url": "/static/media/logo.8b50ef6f.png"
+  }
+]);
